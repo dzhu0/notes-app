71af5ab25c01bc71ea03a89f6be18f5e9b7bff06
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-08-16 18:27:05 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore, collection } from "firebase/firestore"
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDMtYqUkrSIKQzRFVUSFgs2Cfi0yaSL4GM",
+    authDomain: "playground-e3130.firebaseapp.com",
+    databaseURL: "https://playground-e3130-default-rtdb.firebaseio.com",
+    projectId: "playground-e3130",
+    storageBucket: "playground-e3130.appspot.com",
+    messagingSenderId: "263132073559",
+    appId: "1:263132073559:web:be8041d09d69577b23dd12"
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const notesCollection = collection(db, "notes")
