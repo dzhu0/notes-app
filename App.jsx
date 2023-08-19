5ddd6a3f29d3b717067bc7d7f08b75f7ee8832d6
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-08-19 13:24:02 -0500

    Sort notes
    This adds the ability to order the notes in descending order. Focus the current note being edited at the top.

--- App.jsx
+++ App.jsx
@@ -18,6 +18,7 @@ export default function App() {
                 ...doc.data(),
                 id: doc.id
             }))
+            notesArr.sort((a, b) => b.updatedAt - a.updatedAt)
             setNotes(notesArr)
         })
     }, [])
